2b55a88d360f106a3286a8605a0dceb671ebe0f6
William Gonzales <wgonz94@yahoo.com>
2020-04-10 22:06:42 -0700

    added questions and couple functions

--- javascript/app.js
+++ javascript/app.js
@@ -33,10 +33,43 @@ $(document).ready(function(){
 
     let triviaVault = [
         {
-            question: "",
-            answerList: [],
-            answer: "",
-            image: "",
+            question: "What is Spiderman's True Identity?",
+            answerList: ["Tony Stark", "Flash Thompson", "Eddie Brock", "Peter Parker"],
+            answer: 3,
+            // image: "",
+        },
+        {
+            question: "Which one of these characters is NOT a superhero?",
+            answerList: ["Iceman", "The Punisher", "DareDevil", "Black Panther"],
+            answer: 1,
+        },
+        {
+            question: "Who created Ultron?",
+            answerList: ["Tony Stark", "Reed Richards", "Hank Pym", "The Mandarin"],
+            answer: 2,
+        },
+        {
+            question: "What is the Matt Murdocks Occupation?",
+            answerList: ["Detective", "Lawyer", "Boxer", "Judge"],
+            answer: 1,
+        },
+        {
+            question: "Who founded the X-Men?",
+            answerList: ["S.H.E.I.L.D.", "Spiderman", "X-Force", "Charles Xavier"],
+            answer: 3,
+
+        },
+        {
+            question: "Who was the first Super Solider?",
+            answerList: ["Wolverine", "Bucky Barnes", "Steve Rogers", "Natasha Romanova"],
+            answer: 2,
+
+        },
+        {
+            question: "Who is the Leader of the Fantastic 4?",
+            answerList: ["Mr. Fantastic", "Johnny Storm", "Danny Rand", "Dr. Doom"],
+            answer: 0,
+
         }
     ]
 
@@ -44,6 +77,20 @@ $(document).ready(function(){
     //Game Functions
     /////////////////
 
-    
+    //hides game area on initial page load
+    $("#gameCol").hide();
+
+    //OnClick event to start new Game
+    $("#startBtn").on("click", function() {
+        $(this).hide();
+        newGame();
+    })
+
+    //reset button to start new Game
+    $("#resetBtn").on("click", function() {
+        $(this).hide();
+        newGame();
+    })
+
 
 })
